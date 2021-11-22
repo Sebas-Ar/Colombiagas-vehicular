6e81c4fcdafebd7cac21823291d49be79b64259b
Sebas-Ar <juan_ariasd@hotmail.com>
2021-11-22 13:43:02 -0500

    first commit

---
+++ components/social/Whatsapp.jsx
@@ -0,0 +1,94 @@
+/* import { useEffect, useState } from "react";
+import { isMobileOrDesktop } from '../../utils/isMobileOrDesktop' */
+
+const Whatsapp = () => {
+	/* 
+		const [urlWhatsapp, setUrlWhatsapp] = useState("");
+	
+		useEffect(() => {
+			getUrl();
+		}, []);
+	
+		const getUrl = () => {
+			const movileOrDesktop = isMobileOrDesktop();
+			const numero = "573232230714";
+			const msg = "hola ColombiaGas Vehicular";
+			if (movileOrDesktop === "Desktop") {
+				setUrlWhatsapp(
+					`https://web.whatsapp.com/send?phone=${numero}&text=${msg}`
+				);
+			} else {
+				setUrlWhatsapp(
+					`https://api.whatsapp.com/send?phone=${numero}&text=${msg}`
+				);
+			}
+		}; */
+
+	return <button>
+		<div className="message">
+			<p>Chatea con nosotros</p>
+		</div>
+		<a href="https://wa.me/message/NF2C4CFYJIJBG1" target="_blank" rel="noopener noreferrer">
+			<img src="/icon/whatsapp.svg" alt="icono de contacto por whatsapp" />
+		</a>
+
+		<style jsx>{`
+
+			button {
+				position: fixed;
+				z-index: 10;
+				right: 2rem;
+				bottom: 2rem;
+				background-color: unset;
+			}
+
+			img {
+				height: 9rem;
+			}
+
+			.message {
+				position: absolute;
+				background-color: white;
+				border-radius: 1rem;
+				padding: .5rem 1rem;
+				top: -2rem;
+				width: 10rem;
+				animation: enter 2.5s forwards;
+			}
+
+			@keyframes enter {
+				from {
+					right: -30rem;
+				}
+				
+				30% {
+					right: 2rem;
+				}
+
+				40% {
+					right: -2rem;
+				}
+
+				70% {
+					right: 0;
+				}
+
+				90% {
+					transform: scale(1);
+					opacity: 1;
+					right: 0;
+				}
+
+				to {
+					transform: scale(1.3);
+					opacity: 0;
+					right: 0;
+					visibility: none;
+				}
+			}
+
+		`}</style>
+	</button>
+}
+
+export default Whatsapp
